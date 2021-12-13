80f30142f7696e25f7049d7256d47c3b5902e0f8
rodnei94 <rodnei0@gmail.com>
2021-12-13 10:35:41 -0400

    tela inicial

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Inicio from "./components/Inicio"
+
+export default function App() {
+    return <Inicio />
+}
---
+++ src/components/Inicio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Inicio() {
+    return (
+        <div className="telaInicial">
+            <ion-icon name="flash-outline"></ion-icon>
+            <h1>ZapRecall</h1>
+            <div className='praticar'>
+                <h3>Praticar React</h3>
+                <ion-icon name="play-forward"></ion-icon>
+            </div>
+        </div>
+    );
+}
